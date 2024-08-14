1d31e743bfd563535a0f2a5a4a6c1e4da6bb96e3
JuanSeSaCa <156488290+JuanSeSaCa@users.noreply.github.com>
2024-08-14 16:14:17 -0500

    feat: :construction: ajustes finales

--- main.js
+++ main.js
@@ -5,37 +5,37 @@ import { Peliculas } from "./js/modules/peliculas.js";
 
 
 // //* Instanciar el objeto Peliculas
-// let pelicula = new Peliculas();
+ let pelicula = new Peliculas();
 
 // //* Obtener todas las películas disponibles
-  // console.log(await pelicula.getAllFilmsAvailable());
+  console.log(await pelicula.getAllFilmsAvailable());
  
 
 // //* Obtener detalles de una película específica por ID
-// console.log(await pelicula.getAllDetailsFilms({id: "66a6cdd3a7ff449f3519ecdb"}));
+ //console.log(await pelicula.getAllDetailsFilms({id: "66a6cdd3a7ff449f3519ecdb"}));
 
 // //* Instanciar el objeto Clientes
 //let clientes = new Clientes();
 // console.log(clientes.obtenerDetallesUsuario); // Debería mostrar [Function: obtenerDetallesUsuario]
 
 // //* Obtener detalles de un usuario específico por ID
-// console.log(await clientes.obtenerDetallesUsuario({id:'66c48b92d571a320405791ed'}));
+// console.log(await clientes.obtenerDetallesUsuario({id:'66c48b92d571a320405791f3'}));
 
-// //* Obtener todos los clientes
-// console.log(await clientes.findClientes());
+//* Obtener todos los clientes
+//console.log(await clientes.findClientes());
 
 // //* Cambiar Rol de Usuario
 // console.log( await clientes.actualizarRolUsuario({id:'66b28bc85d478ba080d0ff95',nuevoRol:'Administrador'})) 
 
 // //* Crear un nuevo usuario en la base de datos
-// console.log(await clientes.crearUsuario({
-//    nombre: 'Juan Sebastian',
-//    apellido: 'Saenz',
-//    nickname:'JuanSnz',
-//    email: 'juan.saenz1234@correo.com',
-//   telefono: '3506125825',
-//   contrasena: 'juansnz123',
-//   categoria: 'Administrador',
+//  console.log(await clientes.crearUsuario({
+//    nombre: 'Miguel',
+//    apellido: 'Castro',
+//    nickname:'miguelCastro',
+//    email: 'miguelCastro@correo.com',
+//   telefono: '3526166655',
+//   contrasena: 'miguelCastro123',
+//   categoria: 'Usuario Estandar',
 // }));
 
 // //* Instanciar el objeto Funciones
@@ -54,5 +54,5 @@ import { Peliculas } from "./js/modules/peliculas.js";
 //console.log(await clientes.verificarTarjetaVIP({id:'66c48b92d571a320405791ed', numeroTarjeta:'4444333322221111'})); 
 
 
- let asientos = new Asientos()
-console.log(await asientos.findAsientos())
+//let asientos = new Asientos()
+//console.log(await asientos.findAsientos())
