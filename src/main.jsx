53efe807fa858b275d7cbbfcaddca48e4c0757b3
JuanSeSaCa <156488290+JuanSeSaCa@users.noreply.github.com>
2024-08-29 20:07:21 -0500

    feat: :construction: avances de proyecto cine campus, con mvc, react y express
    Ultimo commit donde se muestran los avances del proyecto Cine Campus:   En este commit se han incorporado los siguientes avances en el proyecto Cine Campus, utilizando el patrón de diseño MVC, React y Express:  Implementación de MVC (Modelo-Vista-Controlador): Se han organizado las carpetas del proyecto para seguir el patrón MVC, mejorando la separación de responsabilidades y facilitando el mantenimiento del código.  Modelo (Model): Se han definido los modelos de datos para las diferentes entidades del sistema (como películas, clientes y funciones), utilizando Mongoose para la gestión de los esquemas en MongoDB. Vista (View): Se ha mejorado la estructura de los componentes de React, asegurando una representación clara y dinámica de la interfaz de usuario. Controlador (Controller): Se han creado y optimizado los controladores en Express para manejar las peticiones HTTP y la lógica de negocio. Integración de React y Express: Se ha avanzado en la integración de React para el frontend con Express en el backend, permitiendo una comunicación fluida entre el cliente y el servidor a través de API RESTful.  Mejoras en el manejo de rutas y estados: Se han optimizado las rutas en Express para un manejo más eficiente de las solicitudes, así como el estado de la aplicación en React, utilizando useState y useEffect para la gestión del ciclo de vida de los componentes.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import SeatSelector from './seatSelector.jsx'
+import SeatSelector from './components/seatSelector
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
